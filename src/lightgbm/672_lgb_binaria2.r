52782433a7fa2714ccfeb4d8022867e386d4d9cd
alexchavez1980 <alexchavez1980@gmail.com>
2021-09-19 13:58:52 -0300

    672_lgb_binaria2con_modificaciones

--- src/lightgbm/672_lgb_binaria2.r
+++ src/lightgbm/672_lgb_binaria2.r
@@ -1,3 +1,7 @@
+# MODIFICACIONES ALEX
+# 671_lgb_binaria2 con modificaciones
+# Cambio de semilla y retiro de variables.
+
 #Necesita para correr en Google Cloud
 #16 GB de memoria RAM
 #256 GB de espacio en el disco local
@@ -33,7 +37,8 @@ switch ( Sys.info()[['sysname']],
          Linux   = { directory.root  <-  "~/buckets/b1/" } #Google Cloud
        )
 #defino la carpeta donde trabajo
-setwd( directory.root )
+#setwd( directory.root )
+setwd("D:\\Alex\\Estudio\\Esp_Ciencia_Datos\\02_ MD")
 
 
 
@@ -53,9 +58,9 @@ hs <- makeParamSet(
          makeNumericParam("prob_corte",       lower= 0.020, upper=    0.055)
         )
 
-campos_malos  <- c( "mpasivos_margen" )   #aqui se deben cargar todos los campos culpables del Data Drifting
+campos_malos  <- c( "mpasivos_margen", "mactivos_margen", "mrentabilidad_annual")   #aqui se deben cargar todos los campos culpables del Data Drifting
 
-ksemilla_azar  <- 102191  #Aqui poner la propia semilla
+ksemilla_azar  <- 102229  #Aqui poner la propia semilla
 #------------------------------------------------------------------------------
 #Funcion que lleva el registro de los experimentos
 
